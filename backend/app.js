f6ca1b395d0f648427efc322316e05ef241eb4ff
Alex Paredes <alexparedes83@gmail.com>
2023-02-16 10:02:30 -0700

    first commit

---
+++ backend/app.js
@@ -0,0 +1,50 @@
+//backend app.js
+const express = require('express')
+const cors = require('cors')
+const passport = require("passport")
+const cookieParser = require("cookie-parser")
+const session = require("express-session")
+//----------------------------------------- END OF IMPORTS---------------------------------------------------
+const app = express()
+const db = require('./db')
+
+const port = process.env.PORT || 4000
+const reactClientURL = 'http://localhost:3000' // react client
+
+// Middleware
+app.use(express.static('../client/build'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(
+    cors({
+        origin: reactClientURL, // <-- location of the react app were connecting to
+        credentials: true,
+    })
+)
+app.use(
+    session({
+        secret: "secretcode-pg",
+        resave: false,
+        saveUninitialized: true,
+    })
+)
+app.use(cookieParser("secretcode-pg"))
+app.use(passport.initialize())
+app.use(passport.session())
+require("./auth/passportConfig")(passport)
+
+//----------------------------------------- END OF MIDDLEWARE---------------------------------------------------
+
+app.get('/', (req, res) => {
+    console.log('hi');
+    res.send('hello')
+})
+app.get('/getUsers', db.getUsers)
+
+app.post('/login', passport.authenticate('local', {failureMessage:'not good'}), (req, res)=>{
+    res.send('Authorized')
+})
+
+app.listen(port, () => {
+    console.log(`server is up on port ${port}`)
+})
